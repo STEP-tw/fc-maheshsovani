a963f4a264320b4ea28f4761003a0b02b6c3174b
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2019-01-17 10:58:24 +0530

    Modified hideImage function.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-const displayImage = function() {
-  let jar = document.getElementById('jarImage');
-  jar.style.visibility = 'visible';
-};
-
 const hideImage = function() {
   let jar = document.getElementById('jarImage');
   jar.style.visibility = 'hidden';
-  setTimeout(displayImage, 1000);
+  setTimeout(() => (jar.style.visibility = 'visible'), 1000);
 };
