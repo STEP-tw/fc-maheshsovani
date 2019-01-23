a8848ab34bd7f20b12bafea2c71959f990843ee5
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2019-01-23 15:03:29 +0530

    changed PORT number with process.env.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./src/app.js');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 server.listen(PORT, () => console.log('listening on ', PORT));
